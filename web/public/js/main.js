08f4ddd311f237aa0077c035be2d50d34b213175
Edoardo Rossi <edd.rossi@gmail.com>
2016-01-12 19:02:06 +0100

    Add main JS

---
+++ web/public/js/main.js
@@ -0,0 +1,25 @@
+//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 5000;  //time in ms, 5 second for example
+var $input = $('#myInput');
+
+//on keyup, start the countdown
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+//on keydown, clear the countdown
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
+
+//user is "finished typing," do something
+function doneTyping () {
+  //do something
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("loaded")
+  document.getElementById("user-greeting").textContent = "Welcome";
+});
