daf40886b95bcf0890c09b42ae0a224b446a96e4
Edoardo Rossi <edd.rossi@gmail.com>
2016-01-15 17:53:07 +0100

    May the Ajax be with you

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,16 +1,30 @@
-function doneTyping () {
-  console.log("done...")
+function doneTyping (t) {
+  $.ajax({
+    method: "POST",
+    url: "/elasticbook/suggest",
+    data: { term: t }
+  })
+  .done(function() {
+    console.log( "sent request", t );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log("done...")
+  });
 }
 
 $(document).on('keydown', 'form input[type=text]', function(e) {
   var typingTimer;
-  var doneTypingInterval = 5000;
+  var doneTypingInterval = 2000;
   var $input = $(this);
 
   if (($input.data('suggest') == true)) {
     $input.on('keyup', function () {
       clearTimeout(typingTimer);
-      typingTimer = setTimeout(doneTyping, doneTypingInterval);
+      var term = $input.val()
+      typingTimer = setTimeout(doneTyping(term), doneTypingInterval);
     });
 
     $input.on('keydown', function () {
