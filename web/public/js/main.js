230021453b22bd3624dcc6f0dfd93a96dc4328de
Edoardo Rossi <edd.rossi@gmail.com>
2016-01-18 23:02:09 +0100

    Cleanup the JS

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,32 +1,3 @@
-// function doneTyping (t) {
-//   $.ajax({
-//     method: "POST",
-//     url: "/elasticbook/suggest",
-//     data: { term: t }
-//   })
-//   .done(function(res) {
-//     console.log("received suggestions");
-
-//     var suggestions = [];
-//     var options = res.completion[0]["options"];
-//     options.forEach(function(v) {
-//       suggestions.push(v['text'])
-//     })
-//     console.log(suggestions);
-//     $("form input[type=text][data-suggest=true]").autocomplete({
-//       delay: 300,
-//       minLength: 2,
-//       autoFocus: true,
-//       source: suggestions });
-//   })
-//   .fail(function() {
-//     console.log("error");
-//   })
-//   .always(function() {
-//     console.log("done...")
-//   });
-// }
-
 $(document).ready(function() {
   if (window.location.href.indexOf("/search") == -1) {
     $("form input[type=text][data-suggest=true]").get(0).focus();
@@ -68,21 +39,3 @@ $(document).ready(function() {
       }
     });
 });
-
-// $(document).on('keydown', 'form input[type=text]', function(e) {
-//   var typingTimer;
-//   var doneTypingInterval = 2000;
-//   var input = $(this);
-
-//   if ((input.data('suggest') == true)) {
-//     input.on('keyup', function () {
-//       clearTimeout(typingTimer);
-//       var term = input.val()
-//       typingTimer = setTimeout(doneTyping(term), doneTypingInterval);
-//     });
-
-//     input.on('keydown', function () {
-//       clearTimeout(typingTimer);
-//     });
-//   };
-// });
