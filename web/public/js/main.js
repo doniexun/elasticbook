9523a673bc387d1e3a9d1ae45c1780779bcaa731
Edoardo Rossi <edd.rossi@gmail.com>
2016-01-18 18:36:24 +0100

    Dirty JS

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -6,10 +6,12 @@ function doneTyping (t) {
   })
   .done(function(res) {
     console.log("received suggestions");
-    options = res.completion[0]["options"]
+    var options = res.completion[0]["options"];
+    var suggestions = [];
     options.forEach(function(v) {
-      console.log(v['text'])
+      suggestions.push(v['text'])
     })
+    console.log(suggestions);
   })
   .fail(function() {
     console.log("error");
@@ -19,20 +21,71 @@ function doneTyping (t) {
   });
 }
 
+// new Awesomplete(
+//   $("form input[type=text][data-suggest=true]").get(0),
+//   {
+//     // minChars: 3,
+//     // maxItems: 15,
+//     list: suggestions
+//   });
+
+
+// var awesomplete = new Awesomplete(
+//   input,
+//   {
+//     minChars: 3,
+//     maxItems: 15
+//   });
+// list = [];
+// awesomplete.list = list;
+
 $(document).on('keydown', 'form input[type=text]', function(e) {
   var typingTimer;
   var doneTypingInterval = 2000;
-  var $input = $(this);
+  var input = $(this);
 
-  if (($input.data('suggest') == true)) {
-    $input.on('keyup', function () {
+  if ((input.data('suggest') == true)) {
+    input.on('keyup', function () {
       clearTimeout(typingTimer);
-      var term = $input.val()
-      typingTimer = setTimeout(doneTyping(term), doneTypingInterval);
+      var term = input.val()
+      typingTimer = setTimeout(
+          doneTyping(term),
+          doneTypingInterval);
     });
 
-    $input.on('keydown', function () {
+    input.on('keydown', function () {
       clearTimeout(typingTimer);
     });
   };
 });
+
+// new Awesomplete($('input[type="email"]'), {
+//   list: ["@aol.com", "@att.net", "@comcast.net", "@facebook.com",
+//          "@gmail.com", "@gmx.com", "@googlemail.com", "@google.com",
+//          "@hotmail.com", "@hotmail.co.uk", "@mac.com", "@me.com",
+//          "@mail.com", "@msn.com", "@live.com", "@sbcglobal.net",
+//          "@verizon.net", "@yahoo.com", "@yahoo.co.uk"],
+//   item: function(text, input){
+//     var newText = input.slice(0, input.indexOf("@")) + text;
+//     return Awesomplete.$.create("li", {
+//         innerHTML: newText.replace(RegExp(input.trim(), "gi"), "<mark>$&</mark>"),
+//         "aria-selected": "false"
+//     });
+//     },
+//   filter: function(text, input){
+//     return RegExp("^" + Awesomplete.$.regExpEscape(input.replace(/^.+?(?=@)/, ''), "i")).test(text);
+//     }
+// });
+
+// var ajax
+// ajax = new XMLHttpRequest()
+// ajax.open("GET", "https://restcountries.eu/rest/v1/lang/fr", true)
+// ajax.onload = function() {
+//   {
+//       var list = JSON.parse(ajax.responseText).map(function(i) {
+//       return i.name;
+//       })
+//       new Awesomplete(document.querySelector("#ajax-example input"),{list:list});
+//   }
+// }
+// ajax.send();
